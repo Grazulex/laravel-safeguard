b523b26d9b435adc27d502366c7c0f1df2390053
Jean-Marc Strauven <jms@grazulex.be>
2025-07-17 21:26:00 +0200

    Remove unused migration publishing and command registration from LaravelSafeguardServiceProvider

--- src/LaravelSafeguardServiceProvider.php
+++ src/LaravelSafeguardServiceProvider.php
@@ -16,21 +16,6 @@ final class LaravelSafeguardServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/Config/safeguard.php' => config_path('safeguard.php'),
         ], 'safeguard-config');
-
-        $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations'),
-        ], 'safeguard-migrations');
-
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                Console\Commands\MakeStateMachineCommand::class,
-                Console\Commands\GenerateCommand::class,
-                Console\Commands\ListCommand::class,
-                Console\Commands\ShowCommand::class,
-                Console\Commands\ExportCommand::class,
-                Console\Commands\ValidateCommand::class,
-            ]);
-        }
     }
 
     /**
